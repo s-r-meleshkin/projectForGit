90b7167e8348bf66958cb7f8da56b0b55a6c627c
s-r-meleshkin <2035-s.r.meleshkin@mpt.ru>
2023-05-13 22:22:55 +0300

    project publish

---
+++ src/main/java/com/example/springsecurityapplication/controllers/MainController.java
@@ -0,0 +1,77 @@
+package com.example.springsecurityapplication.controllers;
+
+import com.example.springsecurityapplication.models.Person;
+import com.example.springsecurityapplication.security.PersonDetails;
+import com.example.springsecurityapplication.services.PersonService;
+import com.example.springsecurityapplication.services.ProductService;
+import com.example.springsecurityapplication.util.PersonValidator;
+import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    private final PersonValidator personValidator;
+    private final PersonService personService;
+
+    private final ProductService productService;
+
+    public MainController(PersonValidator personValidator, PersonService personService, ProductService productService) {
+        this.personValidator = personValidator;
+        this.personService = personService;
+        this.productService = productService;
+    }
+
+    @GetMapping("/person account")
+    public String index(Model model){
+        // Получаем объект аутентификации -> с помощью SpringContextHolder обращаемся к контексту и на нем вызываем метод аутентификации. Из сессии текущего пользователя получаем объект, который был положен в данную сессию после аутентификации пользователя
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        String role = personDetails.getPerson().getRole();
+        if(role.equals("ROLE_ADMIN")){
+            return "redirect:/admin";
+        }
+//        System.out.println(personDetails.getPerson());
+//        System.out.println("ID пользователя: " + personDetails.getPerson().getId());
+//        System.out.println("Логин пользователя: " + personDetails.getPerson().getLogin());
+//        System.out.println("Пароль пользователя: " + personDetails.getPerson().getPassword());
+//        System.out.println(personDetails);
+        model.addAttribute("products", productService.getAllProduct());
+        return "/user/index";
+    }
+
+    //    @GetMapping("/registration")
+//    public String registration(Model model){
+//        model.addAttribute("person", new Person());
+//        return "registration";
+//    }
+
+    @GetMapping("/registration")
+    public String registration(@ModelAttribute("person") Person person){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String resultRegistration(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+        personValidator.validate(person, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "registration";
+        }
+        personService.register(person);
+        return "redirect:/person account";
+    }
+
+    @GetMapping("/person account/product/info/{id}")
+    public String infoProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("product", productService.getProductId(id));
+        return "/user/infoProduct";
+    }
+}
